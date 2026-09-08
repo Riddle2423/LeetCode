fcfb3a6d991b410271860874b057cd77e981446c
Mr.Riddle <forbetter2423@gmail.com>
2026-09-08 10:27:35 +0000

    LeetCode Synchronization - Move Zeroes (C++)

--- problems/0283-move-zeroes/0283-move-zeroes.cpp
+++ problems/0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,12 +2,27 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int i = 0;
+int j = 0;
 
-        for (int j = 0; j < nums.size(); j++) {
-            if (nums[j] != 0) {
-                swap(nums[i], nums[j]);
-                i++;
-            }
-        }
+while (i < nums.size() && j < nums.size())
+{
+    
+    while (i < nums.size() && nums[i] == 0)
+        i++;
+
+   
+    while (j < nums.size() && nums[j] != 0)
+        j++;
+
+    if (i > j && i < nums.size() && j < nums.size())
+    {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+
+        i++;
+        j++;
+    }
+}
     }
 };
